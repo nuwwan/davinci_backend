6a8658319fd361639aaa7d7368de2c66d1f17180
Nuwan Rathnayaka <nuwan.cse@gmail.com>
2026-01-22 00:42:04 -0500

    formating and linting changes

--- app/routers/subjectRouter.py
+++ app/routers/subjectRouter.py
@@ -7,13 +7,18 @@ from ..schema import SubjectSchema as schema
 
 router = APIRouter(prefix="/subjects", tags=["Subjects"])
 
+
 @router.post("/", response_model=schema.SubjectResponse)
-def create_subject(subject: schema.SubjectCreate, db: Session = Depends(get_db)):
+def create_subject(
+    subject: schema.SubjectCreate, db: Session = Depends(get_db)
+):
     return SubjectController.create_subject(db, subject)
+
 
 @router.get("/", response_model=list[schema.SubjectResponse])
 def list_subjects(db: Session = Depends(get_db)):
     return SubjectController.get_subjects(db)
+
 
 @router.get("/{subject_id}", response_model=schema.SubjectResponse)
 def get_subject(subject_id: int, db: Session = Depends(get_db)):
@@ -21,6 +26,7 @@ def get_subject(subject_id: int, db: Session = Depends(get_db)):
     if not subject:
         raise HTTPException(status_code=404, detail="Subject not found")
     return subject
+
 
 @router.delete("/{subject_id}")
 def delete_subject(subject_id: int, db: Session = Depends(get_db)):
